ade9f38f4dc2007637d18b7419b1ef4467d8e6a6
gerardobort <gerardobort@gmail.com>
2013-03-07 01:46:14 -0300

    some tweaks

--- src/examples/forces/forces.js
+++ src/examples/forces/forces.js
@@ -1,16 +1,16 @@
 var ball1 = new Cafi.Model({
     mass: 5, 
-    position: [0, 0, -200],
-    velocity: [0, 5, 0],
+    position: [0, 0, 400],
+    velocity: [0, 2, 0],
     debugDomElement: document.getElementById('ball1')
 });
 
-
+/*
 var ball2 = new Cafi.Model({
     mass: 10,
     position: [0, 600, 0],
     velocity: [0, -10, 0],
     debugDomElement: document.getElementById('ball2')
 });
-
+*/
 Cafi.run();
--- src/lib/cafi.js
+++ src/lib/cafi.js
@@ -10,28 +10,61 @@ var Cafi = {
     E: Math.E,
     GRAVITY: 9.81,
     time: 0,
-    dT: 1000/30,
-    timeBreakPoint: 120*1000,
+    dT: 1000/10,
+    timeBreakPoint: 40*1000,
     timeScale: 0.005,
 
     models: [],
+    tiemr: null,
+    initialize: function () {
+        var i, iModel, Cafi__models = Cafi.models;
+        for (i = (Cafi__models || []).length-1; i > -1; --i) {
+            iModel = Cafi__models[i];
+            iModel.render();
+        }
+    },
+    mainLoop: function () {
+        var i, iModel, Cafi__models = Cafi.models;
+        for (i = (Cafi__models || []).length-1; i > -1; --i) {
+            iModel = Cafi__models[i];
+            iModel.process();
+            iModel.render();
+        }
+        
+        if ((Cafi.time += Cafi.dT) > Cafi.timeBreakPoint) {
+            clearInterval(Cafi.timer);
+            console.log('finished');
+        }
+    },
     run: function () {
-        var Cafi__models = this.models;
-        var timer = setInterval(function () {
-            var i, iModel;
-            for (i = (Cafi__models || []).length-1; i > -1; --i) {
-                iModel = Cafi__models[i];
-                iModel.process();
-                iModel.render();
-            }
-            
+        var started = true;
 
-            if ((Cafi.time += Cafi.dT) > Cafi.timeBreakPoint) {
-                clearInterval(timer);
-                console.log('finished');
+        this.initialize();
+        console.log('start')
+        this.timer = setInterval(this.mainLoop, Cafi.dT);
+        window.onkeydown = function (e) {
+            switch (e.keyCode) {
+                case 40: // down arrow
+                    if (started = !started) {
+                        console.log('continue')
+                        Cafi.timer = setInterval(Cafi.mainLoop, Cafi.dT);
+                    } else {
+                        console.log('pause')
+                        clearInterval(Cafi.timer);
+                    }
+                    break;
+                case 37: // left arrow
+                     console.log('rwd')
+                     Cafi.dT *= -1;
+                     Cafi.mainLoop();
+                     Cafi.dT *= -1;
+                    break;
+                case 39: // right arrow
+                     console.log('fwd')
+                     Cafi.mainLoop();
+                    break;
             }
-
-        }, Cafi.dT);
+        };
     }
 };
 
@@ -94,38 +127,44 @@ Cafi.Model.prototype.getResultantForce = function () {
 Cafi.Model.prototype.process = function () {
     var dt = Cafi.dT * Cafi.timeScale,
         p = this.position,
+        p1,
         v = this.velocity,
         m = this.mass,
         f,
-        a;
+        a,
+        ce,
+        pe;
 
-    v = this.velocity;
     f = this.getResultantForce();
     a = [f[0]/m, f[1]/m, f[2]/m];
 
     this.forces = [];
-    this.velocity = [v[0] + a[0]*dt, v[1] + a[1]*dt, v[2] + a[2]*dt];
-    this.position = [p[0] + v[0]*dt, p[1] + v[1]*dt, p[2] + v[2]*dt];
+    this.velocity = v = [v[0] + a[0]*dt, v[1] + a[1]*dt, v[2] + a[2]*dt];
+    this.position = p1 = [p[0] + v[0]*dt, p[1] + v[1]*dt, p[2] + v[2]*dt];
+    this.potentialEnergy = pe = m*Cafi.GRAVITY*p1[2];
+    this.cineticEnergy = ce = 0.5*m*v[2]*v[2];
 
+    //console.log(pe, ce, pe+ce);
 
     // collisions
-    if (this.position[2] <= -390) {
-        this.forces.push([0, 0, -98.9]);
-        var ec = 0.5*m*v[2]*v[2],
-            vAbs1 = Math.sqrt(v[0]*v[0] + v[1]*v[1] + v[2]*v[2]),
-            vAbs2 = Math.sqrt(2*ec/m),
-            vVers = [v[0]/vAbs1, v[1]/vAbs1, v[2]/vAbs1];
-        if (vAbs1 < 0.1)  {
+    if (p1[2] < 100) {
+        var mod_v = Math.sqrt(v[0]*v[0] + v[1]*v[1] + v[2]*v[2]),
+            mod_v2 = Math.sqrt(2*ce/m),
+            ver_v = [v[0]/mod_v, v[1]/mod_v, v[2]/mod_v];
+
+        if (mod_v < 0.1) {
             this.velocity = [0, 0, 0];
         } else {
-            this.velocity = [vVers[0]*vAbs2, vVers[1]*vAbs2, -vVers[2]*vAbs1];
+            //this.velocity = [ver_v[0]*mod_v, ver_v[1]*mod_v, -ver_v[2]*mod_v];
+            this.velocity = [v[0], v[1], -v[2]];
         }
+        this.position = [p[0], p[1], 100];
     }
 };
 
 Cafi.Model.prototype.render = function () {
     this.debugDomElement.style
-        .webkitTransform = 'translate(' + (this.position[1]) + 'px, ' + (-this.position[2]) +'px)';
+        .webkitTransform = 'translate(' + (this.position[1]) + 'px, ' + (this.position[2]) +'px)';
 };
 
 
