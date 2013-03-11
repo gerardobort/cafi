7a9032ac373d187a6278d1434c7d1620e5fdea80
gerardobort <gerardobort@gmail.com>
2013-03-10 23:47:34 -0300

    updating example for gh-page

--- cafi/src/examples/forces/forces.js
+++ cafi/src/examples/forces/forces.js
@@ -1,27 +1,42 @@
 var ball1 = new Cafi.Model({
     mass: 7, 
-    position: [400, 0, 400],
-    velocity: [0, 20, -50]
+    position: [0, 400, 400],
+    velocity: [20, -50, 0]
 });
 
 var ball2 = new Cafi.Model({
     mass: 1,
     position: [400, 400, 400],
-    velocity: [0, -20, -50]
+    velocity: [-20, -50, 0]
 });
 
 var ball3 = new Cafi.Model({
     mass: 3,
-    position: [400, 500, 400],
-    velocity: [0, -30, 44]
+    position: [500, 400, 400],
+    velocity: [-30, 44, 0]
+});
+
+var ball4 = new Cafi.Model({
+    mass: 7, 
+    position: [0, 600, 0],
+    velocity: [60, 0, 60]
 });
 
 document.onmousedown = function (e) {
     var ballx = new Cafi.Model({
         mass: 1,
-        position: [0, e.clientX, 500-e.clientY],
-        velocity: [10, parseInt(Math.random()*50*(e.clientX > window.innerWidth/2 ? -1 : 1), 10), parseInt(Math.random()*100, 10)]
+        position: [e.clientX, e.clientY, 400],
+        velocity: [parseInt(Math.random()*50*(e.clientX > window.innerWidth/2 ? -1 : 1), 10), parseInt(Math.random()*100, 10), -20]
     });
 }
+
+
+document.onmousemove = function (e) {
+    Cafi.containerDomElement.style.webkitTransform = 'translate3d(0, -100px, -600px)'
+        + ' scaleZ(-1)'
+        + ' rotateY(' + ((360/window.innerWidth)*e.clientX*0.5 +270) + 'deg)'
+        + ' rotateX(' + ((360/window.innerHeight)*-e.clientY*0.5 -100) + 'deg)';
+}
+
 
 Cafi.run();
