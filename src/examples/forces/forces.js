8b87104905f452e6f62b61cbd71cf3e184691f3f
gerardobort <gerardobort@gmail.com>
2013-03-28 22:52:02 -0300

    more refactor stuff

--- src/examples/forces/forces.js
+++ src/examples/forces/forces.js
@@ -1,52 +1,56 @@
-var e1 = new Cafi.Model({
-    mass: 1, 
-    charge: 2E-2,
-    position: [250, 300, 200],
-    velocity: [50, 0, 0],
-    name: '1'
-});
+Cafi.loadModules(['v3','model', 'bounding', 'html5'], function () {
 
-var p1 = new Cafi.Model({
-    mass: 1,
-    charge: -2E-2,
-    position: [750, 500, 100],
-    velocity: [-50, 0, 0],
-    name: '2'
-});
-
-var b1 = new Cafi.Model({
-    mass: 10, 
-    position: [250, 500, 200],
-    velocity: [80, 0, 0],
-    name: '1'
-});
-
-var b2 = new Cafi.Model({
-    mass: 10,
-    position: [750, 500, 200],
-    velocity: [-50, 0, 0],
-    name: '2'
-});
-
-
-var i = 2;
-document.onmousedown = function (e) {
-    var px = new Cafi.Model({
-        mass: 1,
-        charge: 1E-2 * (i++%2 ? 1 : -1),
-        position: [e.clientX, e.clientY, 400],
-        velocity: [parseInt(Math.random()*50*(e.clientX > window.innerWidth/2 ? -1 : 1), 10), parseInt(Math.random()*100, 10), -20],
-        name: ''
+    var e1 = new Cafi.Model({
+        mass: 1, 
+        charge: 2E-2,
+        position: [250, 300, 200],
+        velocity: [50, 0, 0],
+        name: '1'
     });
-}
+
+    var p1 = new Cafi.Model({
+        mass: 1,
+        charge: -2E-2,
+        position: [750, 500, 100],
+        velocity: [-50, 0, 0],
+        name: '2'
+    });
+
+    var b1 = new Cafi.Model({
+        mass: 10, 
+        position: [250, 500, 200],
+        velocity: [80, 0, 0],
+        name: '1'
+    });
+
+    var b2 = new Cafi.Model({
+        mass: 10,
+        position: [750, 500, 200],
+        velocity: [-50, 0, 0],
+        name: '2'
+    });
 
 
-document.onmousemove = function (e) {
-    Cafi.containerDomElement.style.webkitTransform = 'translate3d(0, -100px, -600px)'
-        + ' scaleZ(-1)'
-        + ' rotateY(' + ((360/window.innerWidth)*e.clientX*0.5 +270) + 'deg)'
-        + ' rotateX(' + ((360/window.innerHeight)*-e.clientY*0.5 -100) + 'deg)';
-}
+    var i = 2;
+    document.onmousedown = function (e) {
+        var px = new Cafi.Model({
+            mass: 1,
+            charge: 1E-2 * (i++%2 ? 1 : -1),
+            position: [e.clientX, e.clientY, 400],
+            velocity: [parseInt(Math.random()*50*(e.clientX > window.innerWidth/2 ? -1 : 1), 10), parseInt(Math.random()*100, 10), -20],
+            name: ''
+        });
+    };
 
 
-Cafi.run();
+    document.onmousemove = function (e) {
+        Cafi.systemDomElement.style.webkitTransform = 'translate3d(0, -100px, -600px)'
+            + ' scaleZ(-1)'
+            + ' rotateY(' + ((360/window.innerWidth)*e.clientX*0.5 +270) + 'deg)'
+            + ' rotateX(' + ((360/window.innerHeight)*-e.clientY*0.5 -100) + 'deg)';
+    };
+
+
+    Cafi.run();
+
+});
